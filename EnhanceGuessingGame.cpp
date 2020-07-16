65302993400a9d03adf013776239d95b3e9da7d8
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-07-16 19:27:04 -0400

    Create EnhanceGuessingGame.cpp

---
+++ EnhanceGuessingGame.cpp
@@ -0,0 +1,57 @@
+/*  name: jianxin zhu
+    date: july/16/2020
+*/
+
+#include<iostream>
+#include<random>
+#include<ctime>
+using namespace std;
+
+
+int main()
+{
+	default_random_engine engine{ static_cast<unsigned int>(time(0)) };
+	uniform_int_distribution<unsigned int> randomInt{ 1, 1000 };
+
+	char choice;
+
+	do
+	{
+		int guessTime = 1;
+		int guess = 0;
+		int number = randomInt(engine);
+
+		cout << "Enter Your Number: ";
+		cin >> guess;
+
+		while (number != guess)
+		{
+			if (guess > number)
+			{
+				cout << "Too high" << endl;
+			}
+			else if(guess < number)
+			{
+				cout << "Too low" << endl;
+			}
+
+			cout << "Enter Your Number: ";
+			cin >> guess;
+			guessTime++;
+		}
+		if (guessTime < 10)
+		{
+			cout << "You know the secret" << endl;
+		}
+		else
+		{
+			cout << "You should be able to do better!" << endl;
+		}
+
+		cout << "Excellent! You guessed the number!\nWold you like to play agin(y or n)?" << endl;
+		cin >> choice;
+
+	} while (choice != 'n');
+
+	
+}
